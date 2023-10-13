cbd56de73ac6d590df7f068b757cc6fd74b41dca
qkrehf2 <64183602+qkrehf2@users.noreply.github.com>
2023-10-13 20:48:32 +0900

    a

--- src/main/java/net/qf/tutorialmod/TutorialMod.java
+++ src/main/java/net/qf/tutorialmod/TutorialMod.java
@@ -5,6 +5,8 @@ import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.qf.tutorialmod.item.FakeWheat;
 import net.qf.tutorialmod.item.TestPolymerItem;
 
 public class TutorialMod implements ModInitializer {
@@ -16,6 +18,10 @@ public class TutorialMod implements ModInitializer {
         Registry.register(Registries.ITEM,
                 new Identifier("test", "test_item"),
                 new TestPolymerItem(new Item.Settings().fireproof())
+        );
+        Registry.register(Registries.ITEM,
+                new Identifier("test", "fake_wheat"),
+                new FakeWheat(new Item.Settings().maxCount(53).rarity(Rarity.EPIC).fireproof().maxDamage(61))
         );
     }
 }
---
+++ src/main/java/net/qf/tutorialmod/item/FakeWheat.java
@@ -0,0 +1,38 @@
+package net.qf.tutorialmod.item;
+
+import eu.pb4.polymer.core.api.item.PolymerItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class FakeWheat extends Item implements PolymerItem {
+
+    public FakeWheat(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public Item getPolymerItem(ItemStack itemStack, @Nullable ServerPlayerEntity player) {
+        return Items.WHEAT;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        var result =  super.use(world, user, hand);
+
+        if (world.isClient) {
+            return result;
+        }
+
+        user.sendMessage(Text.literal("Hello Worldt!"), false);
+
+        return TypedActionResult.success(result.getValue());
+    }
+}
